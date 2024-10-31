f84ab7d97620b17ccae3b1cf5445b05200693cb3
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-10-31 16:01:51 +0100

    fix

--- start-app.sh
+++ start-app.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x start-app.sh`
 # Process the Nginx configuration template
-#node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
-
-# Run any database migrations
-php artisan migrate --force
-
-# exec php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf
+php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
