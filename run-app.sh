cb815263551bf468a71e82b139e8ecd370b309f7
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-10-31 16:13:14 +0100

    fix

--- run-app.sh
+++ run-app.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x run-app.sh`
-
-# Build assets using NPM
-npm run build
-
-# Clear cache
-php artisan optimize:clear
-
-# Cache the various components of the Laravel application
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
-
-# Run any database migrations
-# php artisan migrate --force
+# Process the Nginx configuration template
+php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
--- start-app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Make sure this file has executable permissions, run `chmod +x start-app.sh`
-# Process the Nginx configuration template
-php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
